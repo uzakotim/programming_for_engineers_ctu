df0a932e131ce003a5cc2ed6792c0993dff1fe44
uzakotim <timauzakov@gmail.com>
2022-03-29 20:54:17 +0200

    adding fibonacci sequence

--- recursion/main.py
+++ recursion/main.py
@@ -1,9 +1,15 @@
 # Created on iPad (Timur).
-
-print ('Hello World!')
 def sum_rec(N):
-    if N == 0:
+    if N ==0:
         return 0
-    return sum_rec(N-1)+N
-
-print(sum_rec(5))
+    return sum_rec(N-1) + N
+def fibonacci(N):
+    if N <= 2:
+        return 1
+    return N-1 + fibonacci(N-2)
+n = int(input("Please enter a number\n"))
+print("Recursive sum: " + str(sum_rec(n)))
+print("Fibonacci sequence: ")
+for i in range(n):
+    print(fibonacci(i+1),end =' ')
+print('')
